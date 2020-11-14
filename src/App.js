fe2ea5949d475f3de5acad7e97094cca56fa5512
Nikita Parfenov <n1koss.boy@gmail.com>
2020-11-15 01:14:56 +0500

    add ru / en locales

--- src/App.js
+++ src/App.js
@@ -1,10 +1,19 @@
 import { Alert } from './components/Alert/Alert';
+import { Text, TranslationsProvider } from '@eo-locale/react';
+import { useSelector } from 'react-redux';
+import { locales } from './locales';
 
-const App = () => (
-  <>
-    <Alert />
-    <h1>Hello React!</h1>
-  </>
-);
+const App = () => {
+  const locale = useSelector((state) => state.app.locale);
+
+  return (
+    <TranslationsProvider language={locale} locales={locales}>
+      <Alert />
+      <h1>
+        <Text id="app.hello" name="React" />
+      </h1>
+    </TranslationsProvider>
+  );
+};
 
 export default App;
---
+++ src/locales.js
@@ -0,0 +1,18 @@
+export const locales = [
+  {
+    language: 'en',
+    messages: {
+      app: {
+        hello: 'Hello {name}! Welcome!',
+      },
+    },
+  },
+  {
+    language: 'ru',
+    messages: {
+      app: {
+        hello: 'Привет {name}! Добро пожаловать!',
+      },
+    },
+  },
+];
